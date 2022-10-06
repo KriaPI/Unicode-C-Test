954790b0c29b3aeb72afb656d801867335347b6f
krijan <kristoffer.berg8@gmail.com>
2022-10-06 18:53:29 +0200

    Renamed and added functions. PSL_utf8_to_utf16 does not work.

--- unicode.c
+++ unicode.c
@@ -27,6 +27,16 @@ SU32_mutable;
 
 typedef struct
 {
+    char16_t* string_content;
+    size_t    length_used;
+    size_t    length_total;
+    size_t    size_allocated;
+    bool      initialized;
+} 
+SU16_immutable;
+
+typedef struct
+{
     const char* string_content;
     size_t      multibyte_chars_total;
     size_t      size_allocated;
@@ -34,7 +44,7 @@ typedef struct
 } 
 SU8_immutable;
 
-size_t SU8_multibyte_length(const char* stringMB, size_t bytes)
+size_t PSL_StringLength_u8(const char* stringMB, size_t bytes)
 {
     // Note: 
     // 1 byte  0xxxxxxx 
@@ -75,7 +85,7 @@ size_t SU8_multibyte_length(const char* stringMB, size_t bytes)
     return length;
 }
 
-SU8_immutable* stringU8_create(const char* stringMB)
+SU8_immutable* PSL_createString_u8(const char* stringMB)
 {
     // Store a string literal within the struct SU8_immutable
     // and populate all struct members with a designated initialization.
@@ -85,7 +95,7 @@ SU8_immutable* stringU8_create(const char* stringMB)
     *object = (SU8_immutable) 
     {
         .string_content        = stringMB,
-        .multibyte_chars_total = SU8_multibyte_length(stringMB, bytes),
+        .multibyte_chars_total = PSL_StringLength_u8(stringMB, bytes)+1,
         .size_allocated        = bytes,
         .initialized           = true
     };
@@ -93,7 +103,7 @@ SU8_immutable* stringU8_create(const char* stringMB)
     return object;
 }
 
-SU8_immutable* stringU8_create_conversion(const char* stringMB, size_t characters, size_t bytes)
+SU8_immutable* PSL_createConversion_u8(const char* stringMB, size_t characters, size_t bytes)
 {
     // Store a string literal within the struct SU8_immutable
     // and populate all struct members with a designated initialization.
@@ -110,7 +120,7 @@ SU8_immutable* stringU8_create_conversion(const char* stringMB, size_t character
     return object;
 }
 
-SU32_mutable* stringU32_create(char32_t* string, size_t characters)
+SU32_mutable* PSL_createString_u32(char32_t* string, size_t characters)
 {
     // Store a string literal within the struct SU8_immutable
     // and populate all struct members with a designated initialization.
@@ -127,8 +137,26 @@ SU32_mutable* stringU32_create(char32_t* string, size_t characters)
     return object;
 }
 
+SU16_immutable* PSL_createString_u16(char16_t* string, size_t characters)
+{
+    // Store a string literal within the struct SU8_immutable
+    // and populate all struct members with a designated initialization.
+    SU16_immutable* object = malloc(sizeof(SU16_immutable)); 
 
-SU32_mutable* SU8_ConvertEncoding(SU8_immutable* stringToConvert)
+    *object = (SU16_immutable) 
+    {
+        .string_content = string,
+        .length_used    = characters,
+        .size_allocated = characters * sizeof(char16_t),
+        .initialized    = true
+    };
+
+    return object;
+}
+
+
+// Tested, does work. 
+SU32_mutable* PSL_utf8_to_utf32(SU8_immutable* stringToConvert)
 {
     size_t utf8_chars        = stringToConvert->multibyte_chars_total;
     size_t utf32_string_size = utf8_chars * sizeof(char32_t);
@@ -136,60 +164,73 @@ SU32_mutable* SU8_ConvertEncoding(SU8_immutable* stringToConvert)
     size_t utf8_index_temp   = 0;
 
     char32_t* utf32_string = malloc(utf32_string_size);
-    char32_t* string_end   = &utf32_string[utf8_chars-1]; 
+    char32_t* string_end   = &utf32_string[utf8_chars]; 
     mbstate_t state;
     memset(&state, 0, sizeof(state));
 
     // Convert each utf-8 code point to utf-32 code points
     for (size_t i=0; i<utf8_chars; i++)
     {
-
         utf8_index_temp = mbrtoc32(&(utf32_string[i]), &(stringToConvert->string_content[utf8_index]),
         (size_t) (string_end - utf32_string[i]) , &state);
+        
         if (utf8_index_temp > 0) utf8_index += utf8_index_temp;
     }
 
     // return the converted string
-    SU32_mutable* converted_string = stringU32_create(utf32_string, utf8_chars);
-    return converted_string;
-}   
-
-SU8_immutable* SU32_ConvertEncoding(SU32_mutable* stringToConvert)
-{
-    // return the converted string
-    mbstate_t state;
-    memset(&state, 0, sizeof(state));
-
-    char* utf8_string = malloc(stringToConvert->length_used * sizeof(char32_t));
-    size_t utf8_index = 0;
-
-    // Convert utf32 code points to utf8 code points
-    for (size_t i=0; i<stringToConvert->length_used; i++)
-    {
-        utf8_index += c32rtomb(&utf8_string[utf8_index], stringToConvert->string_content[i],
-            &state);
-    }
-
-    utf8_string[utf8_index] = '\0';    
-
-    // Create a new string object and return it
-    SU8_immutable* converted_string = stringU8_create_conversion(utf8_string, 
-        stringToConvert->length_used, utf8_index+1);
+    SU32_mutable* converted_string = PSL_createString_u32(utf32_string, utf8_chars);
     return converted_string;
 }
 
+SU16_immutable* PSL_utf8_to_utf16(SU8_immutable* string)
+{
+    size_t u8_chars = string->multibyte_chars_total;
+    size_t u16_allocation_size = u8_chars * sizeof(char16_t);
 
-void SU8_bitPrint(char byteToPrint, char* outputName)
+    char16_t* u16_string = malloc(u16_allocation_size);
+
+
+    size_t conversion_result = 0;
+    size_t processed_bytes = 0;
+    size_t i = 0;
+
+    mbstate_t state;
+    memset(&state, 0, sizeof(state));
+    
+    size_t bytes_to_process = 0;
+    while (processed_bytes < string->size_allocated)
+    {
+        bytes_to_process = (string->size_allocated - processed_bytes);
+
+
+        conversion_result = mbrtoc16(
+            &(u16_string[i]), 
+            &(string->string_content[processed_bytes]), 
+            bytes_to_process,
+            &state
+        );
+
+        if (conversion_result > (size_t) 0) processed_bytes += conversion_result;
+        i++;
+
+    } 
+
+    SU16_immutable* convertedString = PSL_createString_u16(u16_string, i);
+    return convertedString;
+}
+
+
+void PSL_bitPrint(char byteToPrint, char* outputName)
 {
     char oneBitMask = 0b0000001;
     char result;
-    printf("%s: ", outputName);
+    wprintf(L"%s: ", outputName);
     for (char i=7; i>-1; --i)
     {
         result = oneBitMask & (byteToPrint >> i);
-        printf("%d", result);
+        wprintf(L"%d", result);
     }
-    printf("\n");
+    wprintf(L"\n");
 }
 
 
@@ -197,20 +238,24 @@ int main()
 {
     setlocale(LC_ALL, "en_US.utf8");
     
-    SU8_immutable* string = stringU8_create("ååååååååå♥♥♥😂😂😂");
+    SU8_immutable* string = PSL_createString_u8("ååååååååå♥♥♥😂😂😂");
+    SU16_immutable* s16 = PSL_utf8_to_utf16(string);
 
-    SU32_mutable* mutString = SU8_ConvertEncoding(string);
-
-    SU8_immutable* string2 = SU32_ConvertEncoding(mutString);
-
+    /*
+    printf("utf-8:\n");
     for (size_t i=0; i<string->multibyte_chars_total; ++i)
         printf("0x%08X ", string->string_content[i]);
     printf("\n");
-    for (size_t i=0; i<mutString->length_used; ++i)
-        printf("0x%08X ", mutString->string_content[i]);
+    
+    printf("utf-16:\n");
+    for (size_t i=0; i<s16->length_used; ++i)
+        printf("0x%08X ", s16->string_content[i]);
     printf("\n");
+    */
 
+    
+    //wprintf(L"%s", realString);
+    wprintf(L"%ls\n", s16->string_content);
     wprintf(L"%s\n", string->string_content);
-    wprintf(L"%s\n", string2->string_content);
     
 }
